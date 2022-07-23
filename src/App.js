72a6f9151b4d748bd47034d127d9febedbaa12bc
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-23 20:52:55 +0300

    Example with useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [fname, setFname] = useState("Edurd");
+  const [lname, setLname] = useState("Tiutiunnyk");
+  const [age, setAge] = useState(26);
+  const [isBan, setIsBan] = useState(false);
+
+  const handlerChangeData = () => {
+    setLname(prompt("Input Lname"));
+    setFname(prompt("Input Fname"));
+    setAge(Number(prompt("Input Age")));
+  };
+
+  const handlerIsBan = () => {
+    setIsBan(!isBan);
+  };
+
+  const handlerPlusAge = () => {
+    setAge(age + 1);
+  };
+
+  const handlerMinusAge = () => {
+    if (age <= 0) {
+      return;
+    }
+    setAge(age - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>
+        {lname} {fname} {age}
+      </p>
+      <p style={{ backgroundColor: isBan ? "red" : "green", color: "white" }}>
+        {lname} {fname} {isBan ? "is baned" : "is unbaned"}
+      </p>
+      <button onClick={handlerChangeData}>Change Data</button>
+      <button onClick={handlerIsBan}>{isBan ? "Unban" : "Ban"}</button>
+      <button className="disableBtn" onClick={handlerPlusAge} disabled={isBan}>
+        +
+      </button>
+      <button className="disableBtn" onClick={handlerMinusAge} disabled={isBan}>
+        -
+      </button>
+    </>
   );
 }
 
